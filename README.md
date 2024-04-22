8cc8a267007133063873913655c287f1a533e64f
tf63 <tfukuda9031@gmail.com>
2024-04-22 19:44:27 +0900

    impl map base

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
-### next template
+## nextでgoogle map apiをたたく
 
-```
-    # プロジェクトの作成
-    pnpx create-next-app --ts frontend
-    # パッケージのインストール
-    pnpm install
-    # 新規インストール
-    pnpm install <package>
-    # 開発サーバーの立ち上げ
-    pnpm run dev
-```
+パッケージ比較
+- https://npmtrends.com/@react-google-maps/api-vs-google-map-react-vs-google-maps-vs-react-google-maps
+
+マップデザイン
+- https://snazzymaps.com/
+
+### google-map-reactを使う
+https://github.com/google-map-react/google-map-react
+
+### @react-google-maps/apiを使う
+1次リファレンス
+https://github.com/JustFly1984/react-google-maps-api/tree/master/packages/react-google-maps-api
+https://developers.google.com/maps/documentation/javascript/reference?hl=ja
+
+https://qiita.com/min09/items/e25f8d65d5e32bfed80b
+
+https://zenn.dev/t_kitamura/articles/423f023bffe0e1
+
+https://qiita.com/kccs_kazuo_fukudome/items/6e279558037105d2090c
--- frontend/app/react-google-map/page.tsx
+++ frontend/app/react-google-map/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+import { InterfaceMapStyle } from '@/lib/MapStyles'
 
 const containerStyle = {
     width: '1200px',
@@ -12,7 +13,7 @@ const center = {
     lng: 139.7744733
 }
 
-function MyComponent() {
+const MyComponent = () => {
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY!
@@ -20,7 +21,7 @@ function MyComponent() {
 
     const [map, setMap] = React.useState(null)
 
-    const onLoad = React.useCallback(function callback(map) {
+    const onLoad = React.useCallback(function callback(map: any) {
         // This is just an example of getting and using the map instance!!! don't just blindly copy!
         const bounds = new window.google.maps.LatLngBounds(center)
         map.fitBounds(bounds)
@@ -28,12 +29,23 @@ function MyComponent() {
         setMap(map)
     }, [])
 
-    const onUnmount = React.useCallback(function callback(map) {
+    const onUnmount = React.useCallback(function callback(map: any) {
         setMap(null)
     }, [])
 
+    const mapOptions = {
+        styles: InterfaceMapStyle
+    }
+
     return isLoaded ? (
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10} onLoad={onLoad} onUnmount={onUnmount}>
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            options={mapOptions}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+        >
             {/* Child components, such as markers, info windows, etc. */}
             <></>
         </GoogleMap>
